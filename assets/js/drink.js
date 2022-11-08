554d0f276b055ca00cf9937a1dd2033a0e339e73
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-11-08 19:25:06 +0000

    started html and jc code for drink

--- assets/js/drink.js
+++ assets/js/drink.js
@@ -1,1 +1,34 @@
 // Add your code for drink.html and drink.css
+
+var cocktailButton = document.getElementById("cocktail-button");
+var beerButton = document.getElementById("beer-button");
+
+var cocktailSearchHistory = [];
+var beerSearchHistory = [];
+
+function getRandomCocktail () {
+   
+
+}
+
+function getRandomBeer () {
+
+}
+
+
+
+
+
+
+startCocktailSearchHistory ();
+startBeerSearchHistory ();
+
+cocktailButton.addEventListener("click", getRandomCocktail) 
+beerButton.addEventListener("click", getRandomBeer) 
+
+
+// console.log what button they have pressed
+
+// console log later the title of the beer from the api and add it into the beer/cock search history array
+
+// need the entire gets search history and present functions once the main part is done
