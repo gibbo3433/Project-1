12cda901556a854d78f3f26d035ecfa82915b609
aliciaqiao <alicia.qiao11@gmail.com>
2022-11-08 18:22:20 +0000

    Fix type error for not including ingredient.js and create key press handler in ingredient.js

--- assets/js/ingredient.js
+++ assets/js/ingredient.js
@@ -1,1 +1,23 @@
 // Add your code for ingredient.html and ingredient.css
+var userFormEl = document.querySelector('#user-form');
+var meatEl = document.querySelector('#meat');
+var vegEl = document.querySelector('#veg');
+var fruitEl = document.querySelector('#fruit');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var meatname = meatEl.value.trim();
+    var vegname = vegEl.value.trim();
+    var fruitname = fruitEl.value.trim();
+  
+    console.log("meatname ", meatname);
+    console.log("vegname ", vegname);
+    console.log("fruitname ", fruitname);
+
+    meatEl.value = "";
+    vegEl.value = "";
+    fruitEl.value = "";
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
